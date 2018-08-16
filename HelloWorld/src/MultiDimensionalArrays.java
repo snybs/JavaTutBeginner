1d2ec394fccd7fe861b680bb05702a81a2bd9635
ejonsni <jonas.x.snihs@ericsson.com>
2018-08-16 15:42:29 +0200

    First Multi Array Created

--- HelloWorld/src/MultiDimensionalArrays.java
+++ HelloWorld/src/MultiDimensionalArrays.java
@@ -3,7 +3,14 @@ public class MultiDimensionalArrays {
 
 	public static void main(String[] args) {
 
+		int[][] matrix = {
+				{3,45,67},{345,6767,78899}
+		};
 		
+		int value = matrix[0][1];
+		
+		//should print 45
+		System.out.println(value);
 	}
 
 }
