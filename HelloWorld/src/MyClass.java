bc7bda30d9b30111c2c04c10ce73eff30dac39d0
Jonas Snihs <jonas.snihs@gmail.com>
2018-08-17 12:34:53 +0200

    My Class

---
+++ HelloWorld/src/MyClass.java
@@ -0,0 +1,21 @@
+class Person {
+	String name;
+	int age;
+}
+
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		
+		person1.name = "Glen Close";
+		person1.age = 22;
+		
+		Person person2 = new Person();
+		person2.age = 45;
+		person2.name = "Glenn Hysen";
+
+	}
+
+}
