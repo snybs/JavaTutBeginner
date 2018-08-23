8e8116ded153c5cbfc9bac6398a5a39b3570cd9b
Jonas <jonas.snihs@gmail.com>
2018-08-23 15:19:51 +0200

    asdfwe

---
+++ Casting/src/App.java
@@ -0,0 +1,21 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int intValue = 3;
+		short shortValue = 55;
+		byte bytevalue = 4;
+		long longValue = 234234235;
+		float floatValue = 2344.4f;
+		double doubleValue = 22352326;
+		
+		System.out.println(Byte.MAX_VALUE);
+		
+		//cast
+		intValue = (int)longValue;
+		
+
+	}
+
+}
