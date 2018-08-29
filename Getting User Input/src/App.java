c6de0e7339bcfe042074fa36f567efb2cea9cd01
Jonas <jonas.snihs@gmail.com>
2018-08-29 09:14:40 +0200

    asdf

---
+++ Getting User Input/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner input;
+		input = new Scanner(System.in);
+		
+		System.out.println("enter something");
+		String line = input.nextLine();
+		
+		System.out.println("you entered: " + line);
+		
+
+	}
+
+}
